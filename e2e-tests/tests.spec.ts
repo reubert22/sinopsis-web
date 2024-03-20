7254f93338c02b8275f3699db9c6cd0c5500fd58
reubert22 <reubertfernandes@gmail.com>
2024-03-20 01:14:25 -0300

    feat: config playwright

---
+++ e2e-tests/tests.spec.ts
@@ -0,0 +1,76 @@
+import { test, expect, type Page } from '@playwright/test';
+import path from 'path';
+import dotenv from 'dotenv';
+
+import { TRENDING_ALL_URL } from '@/utils/apiConstants';
+
+dotenv.config({
+  path: path.resolve(__dirname, '..', '.env.development.local'),
+});
+
+const awaitHomePage = async ({ page }: { page: Page }) =>
+  await page.goto('http://localhost:3000/');
+
+const getByTestIdAndClick = async ({
+  page,
+  testId,
+}: {
+  page: Page;
+  testId: string;
+}) => {
+  const item = page.getByTestId(testId);
+  await expect(item).toBeVisible();
+  return await item.click();
+};
+
+test('Has title', async ({ page }) => {
+  await awaitHomePage({ page });
+
+  await expect(page).toHaveTitle(/Sinopsis web/);
+});
+
+test('Has redirection to trending', async ({ page }) => {
+  await awaitHomePage({ page });
+
+  await getByTestIdAndClick({ page, testId: 'trending-link' });
+
+  await page.waitForURL('**/trending');
+  await expect(page).toHaveURL(/trending/);
+});
+
+test('Has redirection back to home page', async ({ page }) => {
+  await awaitHomePage({ page });
+
+  await getByTestIdAndClick({ page, testId: 'trending-link' });
+
+  await page.waitForURL('**/trending');
+  await expect(page).toHaveURL(/trending/);
+
+  await getByTestIdAndClick({ page, testId: 'back-button' });
+
+  await expect(page).not.toHaveURL(/trending/);
+});
+
+test('Has redirection to trending and wait items from api', async ({
+  page,
+  request,
+}) => {
+  await awaitHomePage({ page });
+
+  await getByTestIdAndClick({ page, testId: 'trending-link' });
+
+  await page.waitForURL('**/trending');
+  await expect(page).toHaveURL(/trending/);
+
+  const trendingApiCall = await request.get(TRENDING_ALL_URL);
+
+  expect(trendingApiCall.ok()).toBeTruthy();
+  expect(expect.objectContaining(await trendingApiCall.json())).toEqual(
+    expect.objectContaining({
+      page: expect.anything(),
+      results: expect.anything(),
+      total_pages: expect.anything(),
+      total_results: expect.anything(),
+    })
+  );
+});
--- src/utils/apiConstants.ts
+++ src/utils/apiConstants.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL: string = 'https://api.themoviedb.org/3/';
+const API_BASE_URL: string | undefined = process.env.NEXT_PUBLIC_TMDB_BASE_URL;
 const API_KEY: string | undefined = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
 const TRENDING_ALL_URL: string = `${API_BASE_URL}trending/all/day?language=en-US&api_key=${API_KEY}`;
